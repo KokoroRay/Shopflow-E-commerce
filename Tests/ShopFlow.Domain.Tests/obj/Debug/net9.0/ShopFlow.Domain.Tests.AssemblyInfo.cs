61c1199abefb7132c53af9921d770e4221207734
RAY\namng <namnguyen123456cm@gmail.com>
2025-09-13 23:15:58 +0700

    fix and update CI for project

--- Tests/ShopFlow.Domain.Tests/obj/Debug/net9.0/ShopFlow.Domain.Tests.AssemblyInfo.cs
+++ Tests/ShopFlow.Domain.Tests/obj/Debug/net9.0/ShopFlow.Domain.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShopFlow.Domain.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05822162b51dd0b4e523956737a5111ad61d6cf5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e40e0eee7f8bcb4db8a81a70213d8d058317d188")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShopFlow.Domain.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShopFlow.Domain.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
